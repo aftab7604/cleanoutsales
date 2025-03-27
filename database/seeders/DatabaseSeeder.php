93d71351418fed53dcf09683e8016bd3f3228876
devitcoderz <dev.itcoderz@gmail.com>
2025-03-27 21:39:16 +0500

    chng

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            FiltersTableSeeder::class,
+            SalesSeeder::class,
+            SalesFiltersSeeder::class,
+        ]);
         // User::factory(10)->create();
 
         // User::factory()->create([
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,29 +9,29 @@
     <title>@yield('title', 'Clean Out') | EstateBit Real Estate Theme Bootstrap HTML5 Template</title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <!-- Fonts -->
-    <link rel="stylesheet" type="text/css" href="assets/fonts/line-icons.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/fonts/line-icons.css') }}">
     <!-- Slicknav -->
-    <link rel="stylesheet" type="text/css" href="assets/css/slicknav.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/slicknav.css') }}">
     <!-- Color Switcher -->
     {{-- <link rel="stylesheet" type="text/css" href="assets/css/color-switcher.css"> --}}
     <!-- Range Slider -->
-    <link rel="stylesheet" type="text/css" href="assets/css/ion.rangeSlider.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/ion.rangeSlider.css') }}">
     <!-- Range Slider  -->
-    <link rel="stylesheet" type="text/css" href="assets/css/ion.rangeSlider.skinFlat.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/ion.rangeSlider.skinFlat.css') }}">
     <!-- Nivo Lightbox -->
-    <link rel="stylesheet" type="text/css" href="assets/css/nivo-lightbox.css" >
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/nivo-lightbox.css') }}" >
     <!-- Animate -->
-    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/animate.css') }}">
     <!-- Owl carousel -->
-    <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/owl.carousel.css') }}">
     <!-- Main Style -->
-    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/main.css') }}">
     <!-- Tweaks -->
-    <link rel="stylesheet" type="text/css" href="assets/css/tweaks.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/tweaks.css') }}">
     <!-- Responsive Style -->
-    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/responsive.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
     @yield('styles')
@@ -69,26 +69,26 @@
     <!-- End Preloader -->
 
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="assets/js/jquery-min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="{{ asset('assets/js/jquery-min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
     {{-- <script src="assets/js/color-switcher.js"></script> --}}
-    <script src="assets/js/jquery.mixitup.js"></script>
-    <script src="assets/js/jquery.counterup.min.js"></script>
-    <script src="assets/js/ion.rangeSlider.js"></script>
-    <script src="assets/js/jquery.parallax.js"></script>
-    <script src="assets/js/waypoints.min.js"></script>
-    <script src="assets/js/wow.js"></script>
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/jquery.slicknav.js"></script>
-    <script src="assets/js/nivo-lightbox.js"></script>
-    <script src="assets/js/main.js"></script>
-    <script src="assets/js/form-validator.min.js"></script>
-    <script src="assets/js/contact-form-script.min.js"></script>
+    <script src="{{ asset('assets/js/jquery.mixitup.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('assets/js/ion.rangeSlider.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.parallax.js') }}"></script>
+    <script src="{{ asset('assets/js/waypoints.min.js') }}') }}"></script>
+    <script src="{{ asset('assets/js/wow.js') }}"></script>
+    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.slicknav.js') }}"></script>
+    <script src="{{ asset('assets/js/nivo-lightbox.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="{{ asset('assets/js/form-validator.min.js') }}"></script>
+    <script src="{{ asset('assets/js/contact-form-script.min.js') }}"></script>
 
     <!-- Util Scripts -->
-    <script src="assets/js/utils/postalcode.js"></script>
-    <script src="assets/js/utils/utilLib.js"></script>
+    <script src="{{ asset('assets/js/utils/postalcode.js') }}"></script>
+    <script src="{{ asset('assets/js/utils/utilLib.js') }}"></script>
 
     @yield('scripts')
   </body>
